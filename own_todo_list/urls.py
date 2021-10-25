2c1419a4a0dc7dba8f5dc81ec68f3173d2d61b8b
Nabil-Hossain-NBL <nabilhossainprf@gmail.com>
2021-10-25 19:15:34 +0600

    commit

--- own_todo_list/urls.py
+++ own_todo_list/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ]  +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
